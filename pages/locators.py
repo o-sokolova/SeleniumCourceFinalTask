f296ca8015e557fee1c502337d46bbafbfb320a8
o-sokolova <olg_sok@mail.ru>
2023-04-14 23:50:24 +0300

    add product page test

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,15 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_ALERT = (By.CSS_SELECTOR, ".alertinner")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRICE_IN_BASKET = (By.CSS_SELECTOR, ".basket-mini")
---
+++ pages/product_page.py
@@ -0,0 +1,35 @@
+import math
+from selenium.common import NoAlertPresentException
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_button.click()
+
+    def check_add(self):
+        result = self.browser.find_element(*ProductPageLocators.SUCCESS_ALERT)
+        text = result.text
+        expected_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        assert expected_name in text, f"'{expected_name}' should be contained in '{text}'"
+
+    def check_sum(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text.split(" ")[0]
+        basket_price = self.browser.find_element(*ProductPageLocators.PRICE_IN_BASKET).text.split(" ")[3]
+        assert product_price == basket_price, f"product_price={product_price} != basket_price={basket_price}"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
